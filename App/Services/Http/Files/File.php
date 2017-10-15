e1d501c044a9ae6cae66c627efd2456a88665da0
Evgeniy Gordinskiy <evgeniy@gordinskiy.com>
2017-10-15 17:57:58 +0300

    Refactor.

--- App/Services/Http/Files/File.php
+++ App/Services/Http/Files/File.php
@@ -6,20 +6,46 @@ use App\Services\Http\Files\src\Files;
 
 class File extends Files
 {
+
+    protected $file_name;
+    protected $file;
+    public static $store_path_files = '/storage/app/upload';
+    public static $max_file_size = '2000000';
+    public static $allowed_extensions = [];
+    public static $excepted_extensions = [];
+
     public function __construct($file = null)
     {
         if (!$file) {
             $files = parent::__construct();
-            if ( is_array($files)) {
-                foreach ($files as $file) {
-                    return ( new self($file) );
-                }
-            } else {
-                return ( new self($files) );
+            return $files;
+        } else {
+            return ( new self($file) );
+        }
+    }
+
+    private function create($files) {
+        $objects = [];
+        if ( is_array($files) ) {
+            foreach ($files as $i => $file) {
+                $objects[] = $this->init(new self($file));
             }
         } else {
-
+            return ( new self($files) );
         }
 
+        return $objects;
     }
+
+    public function init(File $file)
+    {
+      
+    }
+
+    public function store()
+    {
+        $this->file->move(base_path().static::$store_path_files, $this->file_name);
+        return $this->file_name;
+    }
+    
 }
--- App/Services/Http/Files/src/Files.php
+++ App/Services/Http/Files/src/Files.php
@@ -4,20 +4,57 @@ namespace App\Services\Http\Files\src;
 
 class Files
 {
-    private $files;
+    private $files = [];
     
     public function __construct()
     {
       $this->files = $this->getFiles();
-        
+      $this->check();
     }
     
     private function getFiles() 
     {
-        if( count($_FILES) === 1) {
-            return $_FILES[0];
+        if ( $_FILES ) {
+            return $_FILES;
         }
-        
-        return $_FILES;
+        return false;
+    }
+
+    private function check()
+    {
+        foreach ( $this->files as $file ) {
+        $this->checkFile($file);
+        $this->checkFileSize($file);
+        $ext = $this->checkExtension($file);
+        $this->file_name = $this->file->getFilename() ? $this->file->getFilename(). ".$ext" : uniqid(Auth::user()->id). ".$ext";
+        }
+    }
+
+    public function checkFile($file)
+    {
+        if ( !$request->file || !$request->file->isValid() ) {
+            abort(501,'Your file is broken');
+        }
+        return $request->file;
+    }
+    public function checkFileSize()
+    {
+        if ($this->file->getClientSize() > static::$max_file_size) {
+            abort(413,'Overly large file size',['Accept' => static::$max_file_size]);
+        }
+        return true;
+    }
+    public function checkExtension()
+    {
+        if ( $ext = $this->file->guessExtension() ) {
+            if (static::$allowed_extensions) {
+                if ( !in_array($ext, static::$allowed_extensions) || in_array($ext, static::$excepted_extensions) ) {
+                    abort(510,'' ,['Accept' => implode(',', static::$allowed_extensions)]);
+                }
+            }
+        } else {
+            abort(510,'' ,['Accept' => implode(',', static::$allowed_extensions)]);
+        }
+        return $ext;
     }
 }
