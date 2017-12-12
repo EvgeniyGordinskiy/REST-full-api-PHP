1ec077f0770ae905a1e86b97f801b0a4ff7deacf
EvgeniyGordinksiy <evgeniy@gordinskiy.com>
2017-12-12 23:46:22 +0200

    Add home page

---
+++ public/Frontend/src/app/pages/home/home.js
@@ -0,0 +1,51 @@
+/* ============
+ * Login User
+ * ============
+ *
+ * This is the page for logging user in.
+ */
+
+import auth from './../../services/auth';
+import Forms from './../../utils/forms/forms';
+import Formvue from './../../components/form/form.vue';
+import Header from './../../components/header/header.vue';
+import LayoutV from './../../layouts/user/user.vue';
+
+export default {
+    data() {
+        return {
+            form: new Forms({
+                email: {
+                    value: '',
+                    type: 'email',
+                },
+                password: {
+                    value: '',
+                    type: 'password',
+                },
+            }),
+        };
+    },
+
+    methods: {
+        /**
+         * Logs the user in
+         */
+        login() {
+            this.form.loading = true;
+            auth.login(this.form.data())
+                .catch((errors) => {
+                    console.log(errors.error);
+                    this.form.loading = false;
+                    this.form.recordErrors(errors);
+                });
+        },
+    },
+
+    components: {
+        formv: Formvue,
+        headerv: Header,
+        layoutv: LayoutV,
+
+    },
+};
